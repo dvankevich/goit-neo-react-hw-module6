b52b1757cfd3458a59019e3c0fb56b866678f0f7
dvankevich <dvankevich@gmail.com>
2026-01-02 18:36:46 +0200

    attach redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import "modern-normalize";
 import "./index.css";
 import App from "./components/App.jsx";
+import { store, persistor } from "./redux/store";
 
 createRoot(document.getElementById("root")).render(
-    <StrictMode>
+  <StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
         <App />
-    </StrictMode>
+      </PersistGate>
+    </Provider>
+  </StrictMode>
 );
